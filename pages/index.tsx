4cc1a66184046c7c4f1029f89ac5d0f24d0e4da9
تبادل <ahmad.meshaal.2040@gmail.com>
2026-02-13 16:25:55 +0300

    Create main dashboard page

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Dashboard: React.FC = () => {
+    return (
+        <div>
+            <h1>Main Dashboard</h1>
+            <p>Welcome to the main dashboard page!</p>
+        </div>
+    );
+};
+
+export default Dashboard;
